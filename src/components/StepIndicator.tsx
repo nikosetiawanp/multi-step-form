f89079cde16ae45d3b1ba4e1a27dd1e83995ae65
nikosetiawanp <nikosetiawanp@gmail.com>
2025-07-05 10:01:54 +0700

    fix StepIndicator position

--- src/components/StepIndicator.tsx
+++ src/components/StepIndicator.tsx
@@ -4,21 +4,14 @@ import StepNumber from "./StepNumber";
 
 export default function StepIndicator(props: { currentStep: number }) {
   return (
-    <div className="absolute md:hidden w-full top-0 left-0 -z-50">
-      <img
-        className="absolute z-0 w-full h-auto"
-        src={BackgroundSidebarMobile}
-        alt="bg-sidebar-mobile"
-      />
-      <div className="flex justify-center absolute z-10 top-10 gap-4 w-full">
-        {steps.map((step) => (
-          <StepNumber
-            key={step.number}
-            number={step.number}
-            currentStep={props.currentStep}
-          />
-        ))}
-      </div>
+    <div className="flex justify-center gap-4 w-full my-8 md:hidden">
+      {steps.map((step) => (
+        <StepNumber
+          key={step.number}
+          number={step.number}
+          currentStep={props.currentStep}
+        />
+      ))}
     </div>
   );
 }
